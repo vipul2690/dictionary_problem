ae710a49e90db1ec76780c7f726d49b5e343a0f8
Vipul Lawande <vipullawande@gmail.com>
2019-09-14 15:39:08 +0530

    adding support to get one w ord combo and adding test-cases

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Dictionary
   attr_reader :telephone_number
 
@@ -9,23 +11,21 @@ class Dictionary
     mapping = {'2' => %w(a b c), '3' => %w(d e f), '4' => %w(g h i), '5' => %w(j k l), '6' => %w(m n o),
                '7' => %w(p q r s), '8' => %w(t u v), '9' => %w(w x y z) }
     if is_valid?(@telephone_number)
-      dictionary = %w(catamounts acta mounts act amounts act contour cat boot our)
+      file_path = '../dictionary.txt'
+      dictionary = build_dictionary(file_path)
       digit_to_key = @telephone_number.to_s.chars.map{|digit| mapping[digit]}
       result = {}
-      helper(@telephone_number, 2, digit_to_key, dictionary, result)
+      two_word_combo(@telephone_number, 2, digit_to_key, dictionary, result)
+      result = result.sort.reverse.to_h
       combinations = get_final_words(result)
-      combinations.each {|combo| combo.map(&:inspect).join(', ') }
-      print combinations
-      combinations
-      # num_length = digit_to_key.length
-      # (2..num_length).each { |i|
-      #   combo_one = digit_to_key[0..i]
-      #   combo_two = digit_to_key[i+1..num_length]
-      #   next if combo_one.length < 3 || combo_two.length < 3
-      #   word_array_one = combo_one.shift.product(*combo_one).map(&:join)
-      #   next if word_array_one.nil?
-      #   word_array_two = combo_two.shift.product(*combo_two).map(&:join)
-      # }
+      final_words = []
+      one_word_combos = get_single_words(@telephone_number, digit_to_key, dictionary)
+      final_words << one_word_combos.each { |word| puts word.to_s }
+      combinations.each do |combo|
+        final_words << combo.to_csv.to_s.chop
+      end
+      print final_words
+      final_words
     end
   end
 
@@ -35,16 +35,15 @@ class Dictionary
     flag = (temp_arr.include?('1') || temp_arr.include?('0')) && !(temp_arr.length == 10) ? false : true
   end
 
-  def build_dictionary
+      def build_dictionary(file_path)
     dictionary = []
-    file_path = '../dictionary.txt'
     File.foreach( file_path ) do |word|
       dictionary.push word.chop.to_s.downcase
     end
     dictionary
-  end
+      end
 
-  def helper(input, index, digit_to_key_map, dictionary, results)
+  def two_word_combo(input, index, digit_to_key_map, dictionary, results)
     if index == input.to_s.length
         return
     end
@@ -66,6 +65,13 @@ class Dictionary
       results[index+1] = [(word_store_one & dictionary), (word_store_two & dictionary)]
       helper(input, index+1, digit_to_key_map, dictionary, results)
     end
+  end
+
+  def get_single_words(input, digit_to_key_map, dictionary)
+    input_length = input.to_s.length
+    word_map = digit_to_key_map[0..input_length]
+    words = word_map.shift.product(*word_map).map(&:join)
+    words & dictionary
   end
 
   def get_final_words(results)
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'rspec/mocks'
 require 'dictionary'
 
 RSpec.describe Dictionary  do
@@ -15,6 +16,26 @@ RSpec.describe Dictionary  do
       dictionary = Dictionary.new(1234)
       expect(dictionary.instance_eval{ is_valid?(1234) }).to be false
       expect(dictionary.instance_eval{ is_valid?(02341234555) }).to be false
+    end
+  end
+
+  context 'returns word combinations for number when input is valid' do
+
+    response_one = [["catamounts"], "acta,mounts", "act,amounts", "act,contour", "cat,amounts", "cat,contour"]
+    response_two = [["motortruck"], "motor,truck", "motor,usual", "noun,struck"]
+
+    it 'returns combination of words as array when input is 2282668687' do
+      dictionary = Dictionary.new(2282668687)
+      allow(dictionary).to receive(:is_valid?).with(2282668687).and_return(true)
+      allow(dictionary).to receive(:build_dictionary).with(any_args).and_return( %w(catamounts acta mounts act amounts act contour cat boot our))
+      expect(dictionary.letter_combinations).to eql(response_one)
+    end
+
+    it 'returns combination of words as array when input is 6686787825' do
+      dictionary = Dictionary.new(6686787825)
+      allow(dictionary).to receive(:is_valid?).with(6686787825).and_return(true)
+      allow(dictionary).to receive(:build_dictionary).with(any_args).and_return( %w( motortruck motor truck usual noun struck not opt puck))
+      expect(dictionary.letter_combinations).to eql(response)
     end
   end
 
